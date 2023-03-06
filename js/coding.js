890708c8dfc2f5370395917fe3cfc67aadcf334b
naveedurrahman <rahmannaveed570@gmail.com>
2023-03-06 10:30:54 +0500

    khan folder add to github

---
+++ js/coding.js
@@ -0,0 +1,5 @@
+function changeit()
+        {
+            document.getElementById("box1").innerHTML = "WEB DEVELOPMENT CLASS";
+            document.getElementById("box1").style.background = "navy";
+        }
